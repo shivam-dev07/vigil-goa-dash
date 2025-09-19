feafe22f65b175958bf41440f61555f2248bb70f
MEWTROS <165478153+MEWTROS@users.noreply.github.com>
2025-09-19 21:22:35 +0530

    1 edit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0O2UpaITkdZDJW-Fq0pW59b2uetlhF94",
-  authDomain: "fir-demo-3cec5.firebaseapp.com",
-  projectId: "fir-demo-3cec5",
-  storageBucket: "fir-demo-3cec5.firebasestorage.app",
-  messagingSenderId: "1069490435765",
-  appId: "1:1069490435765:web:2d8806ed37ccc4d74170fc",
-  measurementId: "G-C526R684HQ"
+  apiKey: "AIzaSyASBV1HuSX10cdZJ2GYr6PimsYgYyZwuLg",
+  authDomain: "goapolicefirebase.firebaseapp.com",
+  projectId: "goapolicefirebase",
+  storageBucket: "goapolicefirebase.appspot.com", // usually "<project-id>.appspot.com"
+  messagingSenderId: "892230117968",
+  appId: "1:892230117968:web:6ec95fcf92e2a46496ff55"
 };
 
 // Initialize Firebase
